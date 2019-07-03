c0fa63823a10aad77a038230fa006406f62e06e7
jamagency <36181531+jamagency@users.noreply.github.com>
2019-07-03 21:27:19 +0300

    Create contentReplacerEN.js

---
+++ contentReplacerEN.js
@@ -0,0 +1,77 @@
+<script>
+// header and selector dictionary
+var content = {
+    coctail: [
+    	{
+    	selector: '#manager > div > div.title > div',
+    	content: 'Alexander is pleased with you!'
+    	},
+    	{
+    	selector: '#manager > div > div.entry',
+    	content: '<img width="420" alt="Cocktails" src="/upload/medialibrary/f47/f47abc04c6893dbb6044bd7921ff75ac.jpg" height="67" title="Cocktails">'
+    	}
+    ],
+    moscow: [
+        {
+        selector: '#towns > div',
+        content: 'Moscow number'
+        },
+        {
+        selector: '#manager > div > div.title > div',
+        content: 'Moscow version of the site'
+        }
+    ],
+    headline: [
+        {
+        selector: '#manager > div > div.title > div',
+        content: 'Only the title is replaced'
+        }
+    ]
+};
+
+// заменяет контент
+function replacer(content, utm) {
+    if (utm in content) {
+        for (i in content[utm]) {
+        	if(document.querySelector(content[utm][i]['selector'])!=null) {document.querySelector(content[utm][i]['selector']).innerHTML=content[utm][i]['content'];};
+        };
+    } else {
+        console.log("Каталог контента не имеет такой utm метки");
+    };
+};
+
+
+// возвращает cookie с именем name, если есть, если нет, то undefined
+function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+// записывает utm в cookie на 30 дней
+function setCookie(utm) {
+	var date = new Date(new Date().getTime() + (30*24*60*60*1000));
+	document.cookie = 'utm_replace=' + utm + '; path=/; expires=' + date.toUTCString();
+};
+
+// объединяет все функции в один алгоритм
+function replacerMain(content) {
+	// check is there utm in url
+	if (/utm_replace=([^&]*)/g.exec(document.URL)) {
+		var utm = /utm_replace=([^&]*)/g.exec(document.URL)[1];
+		} else {
+		var utm = null
+	};
+
+	if (utm != null) {
+		replacer(content, utm);
+		setCookie(utm);
+	} else if (getCookie('utm_replace') != undefined) {
+		replacer(content, getCookie('utm_replace'));
+	} else {
+		console.log('UTM replacer не нашел метку ни в URL, ни в cookie')
+	};
+};
+replacerMain(content);
+</script>
