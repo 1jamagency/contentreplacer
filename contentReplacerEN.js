3c9d8e9e44eb8eb4df05777b4a0eb51fa3b01df7
jamagency <36181531+jamagency@users.noreply.github.com>
2022-04-01 15:09:56 +0500

    Update contentReplacerEN.js

--- contentReplacerEN.js
+++ contentReplacerEN.js
@@ -1,48 +1,47 @@
 <script>
-// header and selector dictionary
+// titles and selectors
 var content = {
-   
+    coctail: [
+    	{
+    	selector: '#manager > div > div.title > div',
+    	content: 'Alex is happy with you!'
+    	},
+    	{
+    	selector: '#manager > div > div.entry',
+    	content: '<img width="420" alt="cocktails" src="/upload/medialibrary/f47/f47abc04c6893dbb6044bd7921ff75ac.jpg" height="67" title="cocktails">'
+    	}
+    ],
+    newyork: [
+        {
+        selector: '#towns > div',
+        content: 'New York number'
+        },
+        {
+        selector: '#manager > div > div.title > div',
+        content: 'New York version of the site'
+        }
+    ],
     headline: [
         {
         selector: '#manager > div > div.title > div',
-        content: 'Headline replaced - Good job!!!'
-        }
-    ],
-    washington: [
-        {
-        selector: '#towns > div.phone',
-        content: 'washington phone number'
-        },
-    	{
-        selector: '#manager > div > div.title > div',
-        content: 'Washington website version'
-        }
-    ],
-  	coctail: [
-        {
-        selector: '#manager > div > div.entry > ul > li:nth-child(1)',
-        content: '<img width="420" alt="Cocktails" src="/upload/medialibrary/f47/f47abc04c6893dbb6044bd7921ff75ac.jpg" height="67" title="Cocktails">'
-        },
-    	{
-        selector: '#manager > div > div.title > div',
-        content: 'Alexander is proud of you!'
+        content: 'Only the header is replaced'
         }
     ]
 };
 
-// content replacement
+// Content replacement
 function replacer(content, utm) {
     if (utm in content) {
         for (i in content[utm]) {
         	if(document.querySelector(content[utm][i]['selector'])!=null) {document.querySelector(content[utm][i]['selector']).innerHTML=content[utm][i]['content'];};
         };
     } else {
-        console.log("Content catalog does not contain such UTM-tag");
+        console.log("The resource directory does not have such utm tags");
     };
 };
 
 
-// returns cookie called "name" if exists, or "undefined" - if doesn't
+// returns a cookie named «name», if there is, if not then «undefined»
 function getCookie(name) {
   var matches = document.cookie.match(new RegExp(
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -50,13 +49,13 @@ function getCookie(name) {
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-// writes utm in cookie for 30 days
+// saves utm to cookie for 30 days 
 function setCookie(utm) {
 	var date = new Date(new Date().getTime() + (30*24*60*60*1000));
 	document.cookie = 'utm_replace=' + utm + '; path=/; expires=' + date.toUTCString();
 };
 
-// combines all functions to algorithm
+// combines all functions into one algorithm
 function replacerMain(content) {
 	// check is there utm in url
 	if (/utm_replace=([^&]*)/g.exec(document.URL)) {
@@ -71,7 +70,7 @@ function replacerMain(content) {
 	} else if (getCookie('utm_replace') != undefined) {
 		replacer(content, getCookie('utm_replace'));
 	} else {
-		console.log('UTM replacer have not found tag in URL or cookie')
+		console.log('UTM replacer did not find the tag in either the URL or the cookie')
 	};
 };
 replacerMain(content);
