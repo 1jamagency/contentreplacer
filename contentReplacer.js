de78de90d64d587c4ba51c4d6402a8d5006879a1
jamagency <36181531+jamagency@users.noreply.github.com>
2022-04-01 15:09:36 +0500

    Update contentReplacer.js

--- contentReplacer.js
+++ contentReplacer.js
@@ -1,47 +1,48 @@
+
 <script>
-// titles and selectors
+// калалог заголовков и селекторов
 var content = {
     coctail: [
     	{
     	selector: '#manager > div > div.title > div',
-    	content: 'Alex is happy with you!'
+    	content: 'Сашка доволен доволен тобой!'
     	},
     	{
     	selector: '#manager > div > div.entry',
-    	content: '<img width="420" alt="cocktails" src="/upload/medialibrary/f47/f47abc04c6893dbb6044bd7921ff75ac.jpg" height="67" title="cocktails">'
+    	content: '<img width="420" alt="Коктели" src="/upload/medialibrary/f47/f47abc04c6893dbb6044bd7921ff75ac.jpg" height="67" title="Коктели">'
     	}
     ],
-    newyork: [
+    moscow: [
         {
         selector: '#towns > div',
-        content: 'New York number'
+        content: 'Московский номер'
         },
         {
         selector: '#manager > div > div.title > div',
-        content: 'New York version of the site'
+        content: 'Московская версия сайта'
         }
     ],
     headline: [
         {
         selector: '#manager > div > div.title > div',
-        content: 'Only the header is replaced'
+        content: 'Заменяется только заголовок'
         }
     ]
 };
 
-// Content replacement
+// заменяет контент
 function replacer(content, utm) {
     if (utm in content) {
         for (i in content[utm]) {
         	if(document.querySelector(content[utm][i]['selector'])!=null) {document.querySelector(content[utm][i]['selector']).innerHTML=content[utm][i]['content'];};
         };
     } else {
-        console.log("The resource directory does not have such utm tags");
+        console.log("Каталог контента не имеет такой utm метки");
     };
 };
 
 
-// returns a cookie named «name», if there is, if not then «undefined»
+// возвращает cookie с именем name, если есть, если нет, то undefined
 function getCookie(name) {
   var matches = document.cookie.match(new RegExp(
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -49,28 +50,21 @@ function getCookie(name) {
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-// saves utm to cookie for 30 days 
+// записывает utm в cookie на 30 дней
 function setCookie(utm) {
 	var date = new Date(new Date().getTime() + (30*24*60*60*1000));
 	document.cookie = 'utm_replace=' + utm + '; path=/; expires=' + date.toUTCString();
 };
 
-// combines all functions into one algorithm
+// объединяет все функции в один алгоритм
 function replacerMain(content) {
 	// check is there utm in url
 	if (/utm_replace=([^&]*)/g.exec(document.URL)) {
-		var utm = /utm_replace=([^&]*)/g.exec(document.URL)[1];
-		} else {
-		var utm = null
-	};
-
-	if (utm != null) {
-		replacer(content, utm);
-		setCookie(utm);
+@@ -70,7 +70,7 @@ function replacerMain(content) {
 	} else if (getCookie('utm_replace') != undefined) {
 		replacer(content, getCookie('utm_replace'));
 	} else {
-		console.log('UTM replacer did not find the tag in either the URL or the cookie')
+		console.log('UTM replacer не нашел метку ни в URL, ни в cookie')
 	};
 };
 replacerMain(content);
